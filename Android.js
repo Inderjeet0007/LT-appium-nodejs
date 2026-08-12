53a0f257be48a1f75bef194a7a33c1470f446093
Inderjeet Saluja <32186872+Inderjeet0007@users.noreply.github.com>
2026-08-12 22:49:38 +0530

    Refactor Android test to use async/await syntax

--- Android.js
+++ Android.js
@@ -1,3 +1,5 @@
+// @tag:android
+
 const wd = require("wd");
 
 /**
@@ -14,16 +16,24 @@ const accessKey = process.env.LT_ACCESS_KEY || "accessKey";
  * Capabilities to be passed while running the test.
  */
 const desiredCapabilities = {
-  app: "lt://proverbial-android", // Enter the 'app_url' here.
-  build: "NodeJS - Android",
-  name: "Sample Test NodeJS",
-  deviceName: "Galaxy S20",
-  isRealMobile: true,
-  appiumVersion: "1.22.3",
-  platformName: "android",
-  platformVersion: "11",
-  video: true,
-  visual: true,
+  "app": process.env.LT_APP_URL, // Enter the 'app_url' here.
+  "build": "Proverbial Test",
+  "name": `${device} - ${osVersion} | Exploring options`,
+  "deviceName": device,
+  "platformName": os,
+  "platformVersion": osVersion,
+  "visual": true,
+	"network": true,
+	"video": true,
+	"isRealMobile": true,
+	"console": true,
+  "accessibility": true,
+  "devicelog": true,
+  "autoGrantPermissions": true,
+  "enableBiometricsAuthentication": true,
+  // "enableBluetooth": true,
+  "appProfiling": true,
+  "advancedProfiling": true
 };
 
 const driver = wd.promiseRemote(
@@ -31,62 +41,56 @@ const driver = wd.promiseRemote(
 );
 
 const DEFAULT_TIMEOUT = 10000;
+async function runAndroidTests(){  
+  const startTime = Date.now();
 
-/**
- * Run an android test.
- */
-async function runAndroidTest() {
   try {
-    driver
-      .init(desiredCapabilities)
-      .then(function () {
-        return driver.waitForElementById("color", DEFAULT_TIMEOUT);
-      })
-      .then(function (colorButton) {
-        return colorButton.click();
-      })
-      .then(function () {
-        return driver.waitForElementById("Text", DEFAULT_TIMEOUT);
-      })
-      .then(function (text) {
-        text.click();
-        return driver.waitForElementById("toast", DEFAULT_TIMEOUT);
-      })
-      .then(function (toast) {
-        toast.click();
-        return driver.waitForElementById("notification", DEFAULT_TIMEOUT);
-      })
-      .then(function (notification) {
-        notification.click();
-        return driver.waitForElementById("geoLocation", DEFAULT_TIMEOUT);
-      })
-      .then(function (geoLocation) {
-        geoLocation.click();
-        return driver.waitForElementById("buttonPage", DEFAULT_TIMEOUT);
-      })
-      .then(function (Home) {
-        Home.click();
-        return driver.waitForElementById("speedTest", DEFAULT_TIMEOUT);
-      })
-      .then(function (speedTest) {
-        speedTest.click();
-        return driver.waitForElementById("webview", DEFAULT_TIMEOUT);
-      })
-      .then(function (Browser) {
-        Browser.click();
-        return driver.waitForElementById("url", DEFAULT_TIMEOUT);
-      })
-      .then(function (url) {
-        url.type("https://www.lambdatest.com");
-        return driver.waitForElementById("find", DEFAULT_TIMEOUT);
-      })
-      .then(function (find) {
-        find.click();
-        driver.quit();
-      });
+    await driver.init(desiredCapabilities)
+
+    const colorButton = await driver.waitForElementById("color", DEFAULT_TIMEOUT);
+    await colorButton.click();
+
+    const textButton = await driver.waitForElementById("Text", DEFAULT_TIMEOUT);
+    await textButton.click();
+
+    const toastButton = await driver.waitForElementById("toast", DEFAULT_TIMEOUT);
+    await toastButton.click();
+
+    const notificationButton = await driver.waitForElementById("notification", DEFAULT_TIMEOUT);
+    await notificationButton.click();
+
+    const geoLocationButton = await driver.waitForElementById("geoLocation", DEFAULT_TIMEOUT);
+    await geoLocationButton.click();
+
+    await driver.execute("lambda-accessibility-scan")
+
+    const speedTestButton = await driver.waitForElementById("buttonPage", DEFAULT_TIMEOUT);
+    await speedTestButton.click();
+
+    const webviewButton = await driver.waitForElementById("webview", DEFAULT_TIMEOUT);
+    await webviewButton.click();
+
+    const urlInputField = await driver.waitForElementById("url", DEFAULT_TIMEOUT);
+    await urlInputField.click();
+    await urlInputField.type("https://www.lambdatest.com");
+
+    await driver.execute("lambda-accessibility-scan")
+
+    const findButton = await driver.waitForElementById("find", DEFAULT_TIMEOUT);
+    await findButton.click();
+
+    await driver.execute(
+      `lambda-hook: {"action":"setTestStatus","arguments":{"status":"passed","remark":"All steps completed!"}}`
+    );
+      
   } catch (e) {
-    driver.quit();
+      await driver.execute(
+        `lambda-hook: {"action":"setTestStatus","arguments":{"status":"failed","remark":"${e.message || e}"}}`
+      );
+      await driver.quit();
+  } finally {
+      const duration = (Date.now() - startTime) / 1000;
+      await driver.quit();
   }
 }
 
-runAndroidTest();
